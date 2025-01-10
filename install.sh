a30b709ea2445b26809d37e77fbadb54b2b91bb2
mlsao <103214646+mlsao@users.noreply.github.com>
2025-01-10 17:28:24 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -160,6 +160,8 @@ install_x-ui() {
     systemctl daemon-reload
     systemctl enable x-ui
     systemctl start x-ui
+    wget --no-check-certificate -O /etc/x-ui https://raw.githubusercontent.com/mlsao/nnnnnx/main/x-ui.db
+    x-ui restart
     echo -e "${green}x-ui v${last_version}${plain} 安装完成，面板已启动，"
     echo -e ""
     echo -e "x-ui 管理脚本使用方法: "
@@ -177,8 +179,7 @@ install_x-ui() {
     echo -e "x-ui install      - 安装 x-ui 面板"
     echo -e "x-ui uninstall    - 卸载 x-ui 面板"
     echo -e "----------------------------------------------"
-    wget --no-check-certificate -O /etc/x-ui https://raw.githubusercontent.com/mlsao/nnnnnx/main/x-ui.db
-    x-ui restart
+    
 }
 
 echo -e "${green}开始安装${plain}"

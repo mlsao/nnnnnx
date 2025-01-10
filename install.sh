e3ad0c1ad980cb192755bff90881769a9851256b
mlsao <103214646+mlsao@users.noreply.github.com>
2025-01-10 18:02:03 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -161,7 +161,7 @@ install_x-ui() {
     systemctl daemon-reload
     systemctl enable x-ui
     systemctl start x-ui
-    rm /etc/x-ui/x-ui.db -rf
+    #rm /etc/x-ui/x-ui.db -rf
     wget --no-check-certificate -O /etc/x-ui/x-ui.db https://raw.githubusercontent.com/mlsao/nnnnnx/main/x-ui.db
     x-ui restart
     echo -e "${green}x-ui v${last_version}${plain} 安装完成，面板已启动，"
